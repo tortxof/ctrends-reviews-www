111e974577d51ad1ee2fab37bdb11ea38d01b4f5
Daniel Jones <tortxof@gmail.com>
2015-05-25 02:27:42 -0400

    Added several views and db methods.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from functools import wraps
 import os
+import codecs
 
 from flask import Flask, render_template, flash, session, request, redirect, url_for, jsonify
 
@@ -14,6 +15,9 @@ if not os.path.isfile('key'):
         print(codecs.encode(os.urandom(32), 'hex').decode(), file=f)
 with open('key') as f:
     app.config['SECRET_KEY'] = f.read()
+if os.path.isfile('app.conf'):
+    app.config.from_pyfile('app.conf')
+
 
 db = database.Database('reviews.db')
 
@@ -27,12 +31,12 @@ def login_required(f):
             return redirect(url_for('login'))
     return wrapper
 
-@app.route('setup', methods=['GET', 'POST'])
+@app.route('/setup', methods=['GET', 'POST'])
 def setup():
     if not db.is_new():
         flash('Database is already set up.')
         return redirect(url_for('login'))
-    if request.method = 'POST':
+    if request.method == 'POST':
         appuser = db.new_appuser(request.form.to_dict())
         flash('User {} added.'.format(appuser))
         return redirect(url_for('login'))
@@ -41,14 +45,25 @@ def setup():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method = 'POST':
-        pass
+    if request.method == 'POST':
+        if db.check_password(request.form.get('username'), request.form.get('password')):
+            session['admin'] = request.form.get('username')
+            flash('Logged in as {}.'.format(request.form.get('username')))
+            return redirect(url_for('admin_reviews'))
+        else:
+            flash('Username or password invalid.')
+            return render_template('admin_login.html')
     else:
         return render_template('admin_login.html')
+
+@app.route('/')
+@login_required
+def admin_reviews():
+    return render_template('admin_reviews.html')
 
 @app.route('/reviews.json')
 def get_reviews():
     pass
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
--- database.py
+++ database.py
@@ -10,6 +10,8 @@ class Database(object):
         conn = self.db_conn()
         conn.execute('create table if not exists reviews (id text primary key not null, title, text, author, created)')
         conn.execute('create table if not exists appusers (appuser text primary key not null, password)')
+        conn.commit()
+        conn.close()
 
     def db_conn(self):
         conn = sqlite3.connect(self.dbfile)
@@ -18,4 +20,23 @@ class Database(object):
 
     def is_new(self):
         conn = self.db_conn()
-        return len(conn.execute('select appuser from appusers').fetchall()) == 0
+        num_appusers = len(conn.execute('select appuser from appusers').fetchall())
+        conn.close()
+        return num_appusers == 0
+
+    def new_appuser(self, form):
+        conn = self.db_conn()
+        cur = conn.cursor()
+        password_hash = generate_password_hash(form.get('password'), method='pbkdf2:sha256')
+        cur.execute('insert into appusers values (?, ?)', (form.get('appuser'), password_hash))
+        rowid = cur.lastrowid
+        appuser = conn.execute('select appuser from appusers where rowid=?', (rowid,)).fetchone()[0]
+        conn.commit()
+        conn.close()
+        return appuser
+
+    def check_password(self, appuser, password):
+        conn = self.db_conn()
+        password_hash = conn.execute('select password from appusers where appuser=?', (appuser,)).fetchone()[0]
+        conn.close()
+        return check_password_hash(password_hash, password)
